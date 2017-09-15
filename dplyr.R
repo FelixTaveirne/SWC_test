8fae9cc1873b201c8ba25c9473f89a8b170bd1ee
fjtaveir <fjtaveir@ugent.be>
2017-09-15 15:35:59 +0200

    dyplr

--- dplyr.R
+++ dplyr.R
@@ -1,3 +1,23 @@
 ##### load required library ###
 library(ggplot2)
 library(dplyr)
+
+### load data ###
+df <- read.csv("Metadata.csv",nrow=77)
+stats::filter
+
+mean(df[df$Reactor.phase=="Control","ph"])
+levels(df$Reactor.phase)
+
+#################
+physicochem <- select(df,ph,temp,Conductivity)
+physicochem <- df %>% select(ph,temp,Conductivity) #cmd + shift + m
+
+physicochem.control <- df %>% 
+  filter(Reactor.phase == "Control") %>% select(ph,temp,Conductivity)
+
+####select all diversity's within startup ###
+diversity <- df %>% 
+  filter(Reactor.phase == "Startup") %>% select(Diversity...D0,Diversity...D1,Diversity...D2)
+diversity2 <- df %>% 
+  filter(Reactor.phase == "Startup") %>% select(contains("Diversity"))
