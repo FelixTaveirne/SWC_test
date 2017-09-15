6f6d5081ad8586b686ed67f94dc86d7e95b9ce6c
fjtaveir <fjtaveir@ugent.be>
2017-09-15 15:01:58 +0200

    further alterations

--- SCRIPT.R
+++ SCRIPT.R
@@ -35,8 +35,31 @@ unique(df$Reactor.phase)
 #plot alles in fucntie van reactor phase
 p4 <- p1 + facet_grid(Reactor.phase~Reactor.cycle)
 
-################
-### right side: conductivty
-### middle: diversity DO
-### left: cell density
+###################################
+### right side: conductivty #######
+### middle: diversity DO ##########
+### left: cell density ############
+############### conductivity ######
+df <- read.csv("Metadata.csv",nrows=77)
+pp1 <- ggplot(data=df,aes(x= Timepoint,y=Conductivity,fill=Reactor.phase))
+pp1 <- pp1 + geom_point(shape=21,size=4,alpha = 0.5) + theme_bw() +
+  geom_line(aes(color=Reactor.cycle))
+# Facet it
+pp3 <- pp1 + facet_grid(~Reactor.cycle)
 
+# how do i know whats in reactor phase
+unique(df$Reactor.phase)
+#plot alles in fucntie van reactor phase
+pp4 <- pp1 + facet_grid(Reactor.phase~Reactor.cycle)
+
+#########diversity DO #############
+ppp1 <- ggplot(data=df,aes(x= Timepoint,y=Diversity...D0,fill=Reactor.phase))
+ppp1 <- ppp1 + geom_point(shape=21,size=4,alpha = 0.5) + theme_bw()
+# Facet it
+ppp2 <- ggplot(data=df,aes(x= Timepoint,y=Diversity...D1,fill=Reactor.phase))
+ppp2 <- ppp1 + geom_point(shape=21,size=4,alpha = 0.5) + theme_bw()
+###
+ppp3 <- ggplot(data=df,aes(x= Timepoint,y=Diversity...D3,fill=Reactor.phase))
+ppp3 <- ppp1 + geom_point(shape=21,size=4,alpha = 0.5) + theme_bw()
+
+multiplot(ppp1,ppp2,ppp3,cols=2)
