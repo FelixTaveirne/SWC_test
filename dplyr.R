fa4823156ac48a3df86943603d370795158a60b2
fjtaveir <fjtaveir@ugent.be>
2017-09-16 12:48:36 +0200

    plots

--- dplyr.R
+++ dplyr.R
@@ -21,3 +21,17 @@ diversity <- df %>%
   filter(Reactor.phase == "Startup") %>% select(Diversity...D0,Diversity...D1,Diversity...D2)
 diversity2 <- df %>% 
   filter(Reactor.phase == "Startup") %>% select(contains("Diversity"))
+diversity3 <- df %>% 
+  filter(Reactor.phase == "Startup") %>% select(starts_with("Diversity"))
+#### group by ###
+meanph <- df %>%  group_by(Reactor.phase) %>% summarise(mean.ph=mean(ph),mean.d=mean(Diversity...D0),sd.ph=sd(ph))
+
+######## challenge generate a summary for reactor phase é and add st dev and log 10 transformed cell count
+
+chal <- df %>% filter(Reactor.cycle == 2) %>% group_by(Reactor.phase) %>% summarise(sd_cy=sd(Diversity...D2),mn = mean( log10 (Cell.density..cells.mL.)))
+
+
+######### joining data sets #######
+physicochem <- df %>%  select(sample_title, temp , ph , Conductivity)
+diversity <- df %>%  select(sample_title,contains("Diversity"))
+joint <- full_join(physicochem, diversity, by= "sample_title")
